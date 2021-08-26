ec445154ec35861a2d8c0cf0e93a55c85fc3de8a
Lilly Whalen <lillyw@live.unc.edu>
2021-08-26 14:39:41 -0400

    Update console tests.

--- game_view.js
+++ game_view.js
@@ -24,9 +24,9 @@ export let View = class {
         // Doing something wrong with the dispay of the API or get request?
         //
         var image = new Image();
-        print("ahh!");
+        console.log("ahh!");
         var url = await this.model.getData(select);
-        print(url);
+        console.log(url);
         image.src = url; 
         fact.appendChild(image);
         cell.appendChild(fact);
